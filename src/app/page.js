6a153768ee68bd6542ba0006dabbb23a4f050c3a
thiago-cloud <thiago.154ti@gmail.com>
2025-05-25 18:27:22 -0300

    Adicionado gerenciamento de estado de abas com useState e lógica de filtro
    - Rastrear abas ativas com o estado isActiveTab
    - Implementar alternância de abas via filterProducts
    - Aplicar classe ativa dinâmica ao elemento <li>
    - Estilização do botão dos cards e do titulo além das imagens
    - Adicionei imagem para card para ver como ficaria o formato das imagens dentro dos cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+"use client"
 import { BannerSlider } from "@/components/BannerSlider";
 import { ProductItemV1 } from '@/components/Product_Item_V1';
 
 import Image from "next/image";
+import { useState } from 'react';
 
 export default function Home(){
+
+  const [isActiveTab, setIsActiveTab] = useState(0);
+  const filterProducts = (index) => {
+    setIsActiveTab(index);
+  }
+
   return(
     <>
       <BannerSlider/>
@@ -11,19 +19,19 @@ export default function Home(){
       <section className="filterProducts py-5">
         <div className="container">
           <ul className="list list-inline text-center mb-5">
-            <li className="list-inline-item cursor-pointer">
+            <li className={` list-inline-item cursor-pointer ${isActiveTab===0 && 'active'} `} onClick={ () => filterProducts(0) }>
               <Image src={'/burger.png'} width="70" height={"70"} alt="icon" className="m-auto"/>
               <h4 className="text-lg py-2 mb-0 font-weight-bold">Burgers</h4>
             </li>
-            <li className="list-inline-item  cursor-pointer">
+            <li className={` list-inline-item cursor-pointer ${isActiveTab===1 && 'active'} `} onClick={ () => filterProducts(1) }>
               <Image src={'/dessert.png'} width="70" height={"70"} alt="icon"/>
               <h4 className="text-lg py-2 mb-0 font-weight-bold">Doces</h4>
             </li>
-            <li className="list-inline-item cursor-pointer">
+            <li className={` list-inline-item cursor-pointer ${isActiveTab===2 && 'active'} `} onClick={ () => filterProducts(2) }>
               <Image src={'/salad.png'}  width="70" height={"70"} alt="icon"/>
               <h4 className="text-lg py-2 mb-0 font-weight-bold">Saladas</h4> 
             </li>
-            <li className="list-inline-item  cursor-pointer">
+            <li className={` list-inline-item cursor-pointer ${isActiveTab===3 && 'active'} `} onClick={ () => filterProducts(3) }>
               <Image src={'/sodaAndPotatos.png'} width="70" height={"70"} alt="icon"/>
               <h4 className="text-lg py-2 mb-0 font-weight-bold">Bebidas</h4>
             </li>
--- src/components/Product_Item_V1/index.jsx
+++ src/components/Product_Item_V1/index.jsx
@@ -6,24 +6,34 @@ import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Rating from '@mui/material/Rating';
+import Button from '@mui/material/Button';
+import { IoCartOutline } from "react-icons/io5";
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export const ProductItemV1 = () => {
 
     return(
-        <div className='product_item_v1 p-4'>
-            <div className="imgWrapper relative">
+        <div className='product_item_v1'>
+            <div className="imgWrapper relative  p-3">
                   <Checkbox 
                     {...label} 
                     icon={<FavoriteBorder />} 
                     checkedIcon={<Favorite />} 
-                    className='wishList absolute -top-[0px] -right-[190px] z-[99]' 
+                    className='wishList absolute -top-[0px] -right-[210px] z-[99]' 
                     />
-                <Image src="/panishBurger.jpg" alt="img" width={'100'} height={'100'}/>
+                <Image src="/bigtiBurger.png" alt="img" width={'100'} height={'100'}/>
                 <div className="flex items-center justify-between mt-3">
                     <span className="price flex items-center justify-normal p-2 px-3 py-1 text-y text-lg font-weight-bold bg-brown rounded-md">R$ 10,90</span>
+                     <Rating name="half-rating" defaultValue={2.5} precision={0.5} size="small" readOnly/>
+                
                 </div>
+            </div>
+            <div className='info text-center'>
+                <h4 className="mt-2 font-weight-bold">Bigti Burger</h4>
+                <p className="text-black/60 mt-2 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit mollitia delectus numqua.</p>
+                <Button><IoCartOutline /> Comprar</Button>
             </div>
         </div>
     )
